52ea419e4c6353e8f3bf26a51b956410fd134514
Saikat Das <89715641+CSMLGroup@users.noreply.github.com>
2026-03-23 17:31:22 -0600

    db render

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+}
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
